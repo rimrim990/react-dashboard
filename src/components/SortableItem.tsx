92b5499e89c18acdc668eacf5be76c74b7d328d4
rimrim990 <lim990718@naver.com>
2024-05-07 23:09:36 +0900

    comment

--- src/components/SortableItem.tsx
+++ src/components/SortableItem.tsx
@@ -9,6 +9,11 @@ interface Props {
 
 export default function SortableItem({item}: Props) {
     const {id, tag, content} = item
+
+    /**
+     * draggable & droppable element
+     * - listeners: drag 를 활성화한 장치 이벤트 리스너
+     */
     const {
         attributes,
         listeners,
@@ -16,6 +21,12 @@ export default function SortableItem({item}: Props) {
         transform,
         transition,
     } = useSortable({id});
+
+    /**
+     * transform - sortable element 갱신된 좌표
+     * transition - CSS 모션 속도 조절, 부드럽게 갱신
+     */
+    console.log(`'${id}' transform: x -> ${transform?.x}, y -> ${transform?.y}`)
 
     const style = {
         transform: CSS.Transform.toString(transform),
--- src/components/SortableList.tsx
+++ src/components/SortableList.tsx
@@ -1,25 +1,18 @@
 import React, {useState} from "react";
 import {datas} from "./data";
-import {
-    closestCenter,
-    DndContext,
-    DragEndEvent, DragStartEvent,
-    KeyboardSensor,
-    PointerSensor,
-    useSensor,
-    useSensors
-} from "@dnd-kit/core";
-import {arrayMove, SortableContext, sortableKeyboardCoordinates, verticalListSortingStrategy} from "@dnd-kit/sortable";
+import {closestCenter, DndContext, DragEndEvent, DragStartEvent} from "@dnd-kit/core";
+import {arrayMove, SortableContext, verticalListSortingStrategy} from "@dnd-kit/sortable";
 import SortableItem from "./SortableItem";
 
 export default function SortableList() {
     const [items, setItems] = useState(datas);
-    const sensors = useSensors(
-        useSensor(PointerSensor),
-        useSensor(KeyboardSensor, {
-            coordinateGetter: sortableKeyboardCoordinates,
-        })
-    );
+
+    /**
+     * sensor - 다양한 장치로부터 drag 이벤트 활성화, 기본 값 - Pointer, Keyboard
+     * 1) drag start
+     * 2) drag move
+     * 3) drag end / drag cancel
+     */
 
     const handleDragStart = (event: DragStartEvent) => {
         const {active} = event
@@ -40,8 +33,13 @@ export default function SortableList() {
     }
 
     return (
+        /**
+         * DndContext
+         * - collisionDetection: draggable 과 droppable 의 충돌을 결정하는 알고리름
+         * 1) closestCenter - 중심이 가장 가까운 droppable 컨테이너
+         * 2) Rectangle Intersection - bounding box 가 겹쳐지는 컨테이너
+         */
         <DndContext
-            sensors={sensors}
             collisionDetection={closestCenter}
             onDragStart={handleDragStart}
             onDragEnd={handleDragEnd}
@@ -55,6 +53,13 @@ export default function SortableList() {
                 </tr>
                 </thead>
                 <tbody>
+
+                {
+                    /**
+                     * useSortable 훅에서 사용할 context 제공
+                     * - transform 계산을 위한 여러 가지 전략을 전달받음
+                     */
+                }
                 <SortableContext
                     items={items}
                     strategy={verticalListSortingStrategy}
